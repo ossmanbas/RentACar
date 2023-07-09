79222cd239cc0550556556e672cb1d7237f6dd08
Osman Baş <83305672+ossmanbas@users.noreply.github.com>
2023-07-09 22:00:56 +0300

    Modeller Generic yapıya taşındı. Htmllerde revizeler yapıldı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { CarComponent } from './components/car/car.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:CarComponent}, // Anasayfada hiç bişey açılmazsa Car Componenti aç. path = Default açılacak sayfa.
+  {path:"cars",component:CarComponent} //path domain isminden sonra gelecek sayfa. wwww.blabla.com/cars
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
